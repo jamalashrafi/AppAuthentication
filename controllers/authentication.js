7428d61a78febdbefe8abe1abe1b223730034d85
Jamal Nabi <Jamalnabi07@gmail.com>
2020-03-07 17:04:09 +0530

    initial commit

---
+++ controllers/authentication.js
@@ -0,0 +1,56 @@
+const User = require("../models/user.js");
+const jwt = require('jwt-simple');
+const config = require('../config.js')
+
+function tokenForUser(user){
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat: timestamp, }, config.secret);
+}
+
+exports.signin = function(req, res, next){
+    //User has already had their email and password auth'd
+    //We just need to give them a token
+
+    res.send({ token : tokenForUser(req.user)});
+}
+
+exports.signup = function(req, res, next){
+    
+    const email = req.body.email;
+    const password = req.body.password;
+    //res.send({ sucess : 'true'});
+
+    if(!email || !password){
+        return res.status(422).send(' You must have email and password')
+    }
+
+    User.findOne( { email : email }, (err, existingUser) => {
+ 
+        //See it the user with the given email exists
+        if(err){ return next(err); }
+
+       //If the the email does exists return an error
+        if(existingUser){
+            return res.status(422).send(' User already exists');
+        }
+       // else{
+        //If the email does not exists create and save the user 
+        const user = new User({
+           email : email,
+           password : password
+        });
+
+        user.save((err)=>{
+            if(err){ return next(err);}
+        //Respond the request with the email created signal
+            res.json({ token : tokenForUser(user)});
+        });
+        //}
+    });
+
+    
+
+     
+
+    
+}
